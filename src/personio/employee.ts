8db64d18e494ea2b7d65473e8e051ff414f903a7
William Dry <wpbdry@gmail.com>
2020-09-29 19:20:20 +0200

    fix personio.Employee type

--- src/personio/employee.ts
+++ src/personio/employee.ts
@@ -1,45 +1,47 @@
 export type Supervisor = {
     type: 'Employee'
     attributes: {
-      id: {
-        label: 'ID'
-        value: number
-      }
-      first_name: {
-        label: 'First name'
-        value: string
-      }
-      last_name: {
-        label: 'Last name'
-        value: string
-      }
-      email: {
-        label: 'Email'
-        value: string
-      }
+        id: {
+            label: 'ID'
+            value: number
+        }
+        first_name: {
+            label: 'First name'
+            value: string
+        }
+        last_name: {
+            label: 'Last name'
+            value: string
+        }
+        email: {
+            label: 'Email'
+            value: string
+        }
     }
 }
 
 export type Employee = Supervisor & {
-    status: {
-        label: 'Status'
-        value: string
-    }
-    position: {
-        label: 'Position'
-        value: string
-    }
-    supervisor: {
-        label: 'Supervisor'
-        value: Supervisor | null
-    }
-    hire_date: {
-        label: 'Hire date'
-        value: string
-    }
-    department: {
-        label: 'Department'
-        value: Department | null
+    attributes: {
+        status: {
+            label: 'Status'
+            value: string
+        }
+        position: {
+            label: 'Position'
+            value: string
+        }
+        supervisor: {
+            label: 'Supervisor'
+            value: Supervisor | null
+        }
+        hire_date: {
+            label: 'Hire date'
+            value: string
+        }
+        department: {
+            label: 'Department'
+            value: Department | null
+        }
     }
 }
 
